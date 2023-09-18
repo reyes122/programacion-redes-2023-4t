fe89cc1ff495d7d9822e241bb95ed8b465058f2f
reyes122 <bryanibarra959@gmail.com>
2023-09-18 11:42:09 -0600

    Lab 2.1.1.6 2.1.1.18 2.1.1.19

---
+++ Modulo 2/Lab_2_1_1_19.py
@@ -0,0 +1,37 @@
+
+
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *****")
+
+
+print("         *\n        * *\n       *   *\n      *     *\n     *       *\n    *         *\n   *           *\n  *****     *****\n      *     *\n      *     *\n      *     *\n      *     *\n      *     *\n      *******")
+
+
+print("         *       "*2)
+print("        * *      "*2)
+print("       *   *     "*2) 
+print("      *     *    "*2)
+print("     *       *   "*2)
+print("    *         *  "*2)
+print("   *           * "*2)
+print("  *****     *****"*2)
+print("      *     *    "*2)
+print("      *     *    "*2)
+print("      *     *    "*2)
+print("      *     *    "*2)
+print("      *     *    "*2)
+print("      *******    "*2)
+
+print("         *       "*2)
+print("        * *      "*2)
+print("       *   *     "*2) 
+print("      *     *    "*2)
+print("     *       *   "*2
+print("    *         *  "*2)
+print("   *           * *2)
+print("  *****     *****"*2)
+print("      *     *    '*2)
+print("      *     *    "*2)
+print("      *     *    "*2)
+print("      *     *    "*2)
+print("      *     *    "*2)
+print("      *******    "*2)
